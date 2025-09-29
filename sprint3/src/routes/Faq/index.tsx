2ef023f556726849daeac417b66a88cb1861dacf
EnzoVazz <evaz0711@gmail.com>
2025-09-29 12:00:49 -0300

    Criando o FAQ

--- sprint3/src/routes/Faq/index.tsx
+++ sprint3/src/routes/Faq/index.tsx
@@ -1,92 +1,29 @@
-function Faq() {
-  return (
-    <div className="faq">
-      {" "}
-     
-        <div className="retangulobody"></div>{" "}
-      <div className="retanguloperguntasfrequentes"></div>{" "}
-      <div className="perguntasfrequentestitulo">PERGUNTAS FREQUENTES</div>{" "}
-      <div className="perguntas">
-        {" "}
-        <ol className="perguntas-span">
-          {" "}
-          <li>
-            {" "}
-            O que é o site Conexão HC e qual sua relação com o HC?{" "}
-            <span className="perguntas-span3">
-              {" "}
-              <br /> R:{" "}
-            </span>{" "}
-            <span className="perguntas-span5">
-              {" "}
-              O Conexão HC é um site de apoio criado para auxiliar pacientes e
-              usuários do Hospital das Clínicas com informações práticas,
-              orientações e facilitação de acesso a serviços. Não substituímos o
-              site oficial do HC, mas atuamos como um complemento para tornar
-              sua experiência mais simples e informativa.{" "}
-            </span>{" "}
-          </li>{" "}
-          <li>
-            {" "}
-            Como entro em contato com o HC diretamente?{" "}
-            <span className="perguntas-span3">
-              {" "}
-              <br /> R:{" "}
-            </span>{" "}
-            <span className="perguntas-span5">
-              {" "}
-              No Conexão HC, disponibilizamos todos os telefones, redes sociais
-              e canais de atendimento digital do HC atualizados para facilitar
-              seu contato com as unidades ou setores desejados, basta só acessar
-              a página de contatos.{" "}
-            </span>{" "}
-          </li>{" "}
-          <li>
-            {" "}
-            Preciso ter cadastro para usar a Conexão HC?{" "}
-            <span className="perguntas-span3">
-              {" "}
-              <br /> R:{" "}
-            </span>{" "}
-            <span className="perguntas-span5">
-              {" "}
-              Sim. Para agendar ou remarcar consultas e utilizar todas as outras
-              funcionalidades da plataforma, é necessário criar uma conta com
-              login e senha.{" "}
-            </span>{" "}
-          </li>{" "}
-          <li>
-            {" "}
-            Posso agendar consultas diretamente pela plataforma?{" "}
-            <span className="perguntas-span3">
-              {" "}
-              <br /> R:{" "}
-            </span>{" "}
-            <span className="perguntas-span5">
-              {" "}
-              Sim. Através do nosso sistema, você pode agendar consultas em
-              unidades vinculadas ao HC, escolher especialidades disponíveis e
-              acompanhar o status dos seus atendimentos.{" "}
-            </span>{" "}
-          </li>{" "}
-          <li>
-            {" "}
-            Quem posso procurar se tiver problemas para acessar ou agendar?{" "}
-            <span className="perguntas-span3">
-              {" "}
-              <br /> R:{" "}
-            </span>{" "}
-            <span className="perguntas-span5">
-              {" "}
-              Nossa equipe de suporte está disponível via chat no site ou pela
-              página de contatos disponível aqui para ajudar com questões
-              técnicas, login, agendamento e uso da plataforma.{" "}
-            </span>{" "}
-          </li>{" "}
-        </ol>{" "}
-      </div>{" "}
-     
-    </div>
-  );
+import FaqAberto from "../../components/FaqAberto/FaqAberto";
+import { faqData } from "../../data/DataFaq";
+
+export default function FAQ() {
+    return (
+        <div className="min-h-screen bg-white py-10">
+            <div className="max-w-xl mx-auto px-4">
+                
+                
+                <h1 className="text-2xl font-medium text-center text-black mb-8 p-6 bg-[#acf0ff] rounded-lg shadow-md border-2 border-[#169da6]">
+                    PERGUNTAS FREQUENTES
+                </h1>
+                
+                
+                <div className="bg-[#acf0ff] rounded-lg shadow-xl overflow-hidden divide-y divide-[#169da6] border-2 border-[#169da6]">
+                    
+                    {faqData.map(item => (
+                        <FaqAberto 
+                            key={item.id} 
+                            item={item}
+                        />
+                    ))}
+                    
+                </div>
+                
+            </div>
+        </div>
+    );
 }
-export default Faq;
