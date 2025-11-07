d1f4d93b8bacda7ef4b62d854ae45ebf83090c58
EnzoVazz <evaz0711@gmail.com>
2025-11-07 14:37:49 -0300

    fix: Arrumando o app.tsx

--- sprint4/src/App.tsx
+++ sprint4/src/App.tsx
@@ -6,24 +6,12 @@ import { useState } from "react";
 
 export default function App(){
     
-    let nomeUsuarioLogado = "Usuário"; 
-    const usuarioJson = localStorage.getItem('usuarioLogado'); 
-
-    if (usuarioJson) {
-        try {
-            const usuario = JSON.parse(usuarioJson);
-            nomeUsuarioLogado = usuario.nome || "Usuário"; 
-        } catch (e) {
-            console.error("Erro ao fazer parse do usuário no localStorage:", e);
-        }
-    }
     
     const [menuAberto, setMenuAberto] = useState(false);
     
     const toggleMenu = () => {
         setMenuAberto(!menuAberto);
     };
-
 
     return(
         <div className="min-h-screen flex flex-col"> 
