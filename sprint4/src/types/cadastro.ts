ab9d97a645412631eb8f6d90c90d2db298ae1724
EnzoVazz <evaz0711@gmail.com>
2025-11-06 21:21:47 -0300

    feat: adicionando types da api /cadastro

--- sprint4/src/types/cadastro.ts
+++ sprint4/src/types/cadastro.ts
@@ -1,9 +1,20 @@
+
+export type EnderecoForm = {
+  logradouro: string;
+  numero: number;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  cep: string;
+};
+
 export type CadastroForm = {
-    nome: string;
-    email: string;
-    dataNascimento: string;
-    telefone: string;
-    cpf: string;
-    senha: string;
-    confirmarSenha: string; 
+  nome: string;
+  email: string;
+  dataNascimento: string;
+  telefone: string;
+  cpf: string;
+  senha: string;
+  confirmarSenha: string; 
+  endereco: EnderecoForm; 
 };
