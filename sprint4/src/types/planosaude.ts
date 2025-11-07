0244040972e31ad9794b03213b9e6ad684ce6f65
LucasFukuda2408 <luquinhasryuji@gmail.com>
2025-11-07 12:32:07 -0300

    feat: criando types para pagina de plano de saude

---
+++ sprint4/src/types/planosaude.ts
@@ -0,0 +1,11 @@
+export type PlanoSaudeForm = {
+  nomePlano: string;
+  numeroCarteirinha: number;
+  validade: string;
+};
+
+export type PlanoSaude = {
+  nomePlano: string;
+  numeroCarteirinha: number;
+  validade: string;
+};
